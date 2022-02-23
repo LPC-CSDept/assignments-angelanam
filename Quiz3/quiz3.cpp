798eb262d6d624a0e183c79417268b9b03737aa9
angelanam <ahnam@zonemail.clpccd.edu>
2022-02-22 22:36:23 -0800

    commit

--- Quiz3/quiz3.cpp
+++ Quiz3/quiz3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <iomanip>
+#include <cstdlib> //standard library
+#include <ctime> //
+#include <iomanip> //
 using namespace std;
 
 int main ()
@@ -9,19 +9,19 @@ int main ()
     int random1, random2, random3;
 	srand(time(0));
 	
-	random1 = rand() % 100 ;
-	cout << "Random number 1: " << random1 << endl;
+	random1 = rand() % 100 ; //generates random number between 0-99
+	cout << "Random number 1: " << random1 << endl; //prints random #1
 
-	random2 = rand() % 100 ;
-	cout << "Random number 2: " << random2 << endl;
+	random2 = rand() % 100 ; //0-99
+	cout << "Random number 2: " << random2 << endl; //prints random #2
 
-    random3 = rand() % 100 ;
-    cout << "Random number 3: " << random3 << endl;
+    random3 = rand() % 100 ; //0-99
+    cout << "Random number 3: " << random3 << endl; //prints random #3
 
-	int sum = random1 + random2 + random3;
-    double avg = sum / 3.0;
+	int sum = random1 + random2 + random3; //adds total number of the random numbers
+    double avg = sum / 3.0; //dividing total by 3 to calculate average
 
-    cout << setprecision(5) << fixed;
-    cout << "Total amount: " << sum << endl;
-    cout << "Average amount: " << avg << endl;
+    cout << setprecision(5) << fixed; //sets average amount in 5 decimal places
+    cout << "Total amount: " << sum << endl; //prints total amount of the numbers
+    cout << "Average amount: " << avg << endl; //prints average amount 5 decimal places
 }
