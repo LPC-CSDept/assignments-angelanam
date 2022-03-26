d9610fa86ba5a0801e1b55b383d1f1b3609aa908
angelanam <ahnam@zonemail.clpccd.edu>
2022-03-25 19:03:38 -0700

    commit

---
+++ Quiz5-2/question1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    const int N=5;
+	int 	rdnum;
+	int 	min;
+    
+    srand(time(0));
+
+	for(int i=0;i<N;i++){
+		rdnum = rand() % 100 ;	
+		cout << rdnum << "\t"; 
+		if ((i==0) || ( min > rdnum))
+			min = rdnum;
+	}
+	cout << "The minimum number is: " << min << endl;
+}
