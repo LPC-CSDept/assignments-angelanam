0adf0e5c0b7fcecb89380c1e36c0bcee58de73da
Kyuwoong Lee <kwooler@gmail.com>
2022-03-27 13:09:43 -0700

    5-2

--- Quiz5-2/question2.cpp
+++ Quiz5-2/question2.cpp
@@ -6,7 +6,16 @@ int main()
     const int N=3;
 	const int M=2;
 
-	for (int i=0; i<N<10;)
-		for(int j=1; j<=M<=10;)
-			cout << i << " " << j  << endl;
+
+	// Hope you pratice more about "for-loop" Structure
+	// Read the lecture slides and pratice all example programs and programming labs.
+	// basic form of for-loop, like
+	for (int i=0; i<10; i++)
+	{
+
+	}
+	
+// for (int i=0; i<N<10;)
+//		for(int j=1; j<=M<=10;)
+//			cout << i << " " << j  << endl;
 }
