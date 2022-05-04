43786e8bb65b29c38bd617f32884677cc9f2c143
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-03 23:00:47 -0700

    commit10

--- Quiz5-2/question4read.cpp
+++ Quiz5-2/question4read.cpp
@@ -21,9 +21,16 @@ int main()
     << "Salary\t" << endl;
 
     sum = 0;
-    for (int i=0; i<num; i++){
+    for (int i=0; i<num; i++){ 
+    //prints each line of employee info based on # of employees
         ifs >> id >> name >> dept >> salary;
-        sum += salary;
-        cout << id << "\t" << 
+        sum += salary; 
+        //adds all salaries based on # of employees
+        cout << id << "\t" << name << "\t" << dept "\t" 
+        << salary << "\t" endl;
     }
+
+    avg = sum / num;
+    cout << "Total salary: " << sum << "\t" <<
+    "Total average: " << avg << "\t" << endl;
 }
