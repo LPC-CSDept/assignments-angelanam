c9605d8434da49780b45c8f89a3b592797b52e29
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-03 19:20:34 -0700

    commit2

--- Quiz5-2/question1.cpp
+++ Quiz5-2/question1.cpp
@@ -5,16 +5,18 @@ using namespace std;
 
 int main()
 {
-    const int N=5;
+    const int N=5;   //set to 5 to print 5 #'s
 	int 	rdnum;
 	int 	min;
     
     srand(time(0));
 
+	cout << "5 random numbers: " << endl;
+
 	for(int i=0;i<N;i++)
     {
-		rdnum = rand() % 100;	
-		cout << rdnum << "\t";
+		rdnum = rand() % 100; //generates random #'s 0-100
+		cout << rdnum << ", \t";
 		if ((i==0) || ( min > rdnum))
 			min = rdnum;
 	}
