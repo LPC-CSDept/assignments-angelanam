6099831138be4309973f3f7adf4590c1ed388817
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-03 19:32:49 -0700

    commit4

--- Quiz5-2/question2.cpp
+++ Quiz5-2/question2.cpp
@@ -5,15 +5,20 @@ int main()
 {
     int N, M;
 	const int min=0, max=10;
-	int result=1; //set to 1 to multiply base number by itself 
+	int result=1; 
+	//set to 1 to multiply base number by itself 
 
 	cout << "Enter the base number: ";
 	cin >> N;
 	cout << "Enter the exponent: ";
 	cin >> M;
 
+	cout << "Each power of " << N << "^" << 
+    M << " is:\n";
+
 	if (N<max && N>min){
-		for (int i=0; i<M; i++) //loop creates each exponent amount
+		for (int i=0; i<M; i++) 
+		//loop creates each exponent amount
 		{
 			result *= N;
 			cout << result << endl;
