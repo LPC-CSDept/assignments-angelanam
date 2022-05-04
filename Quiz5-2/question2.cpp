74e1ca4f50cb5ad9b7e7a818390062a373f61893
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-03 19:31:37 -0700

    commit3

--- Quiz5-2/question2.cpp
+++ Quiz5-2/question2.cpp
@@ -3,19 +3,22 @@ using namespace std;
 
 int main()
 {
-    const int N=3;
-	const int M=2;
+    int N, M;
+	const int min=0, max=10;
+	int result=1; //set to 1 to multiply base number by itself 
 
+	cout << "Enter the base number: ";
+	cin >> N;
+	cout << "Enter the exponent: ";
+	cin >> M;
 
-	// Hope you pratice more about "for-loop" Structure
-	// Read the lecture slides and pratice all example programs and programming labs.
-	// basic form of for-loop, like
-	for (int i=0; i<10; i++)
-	{
-
+	if (N<max && N>min){
+		for (int i=0; i<M; i++) //loop creates each exponent amount
+		{
+			result *= N;
+			cout << result << endl;
+		}
 	}
-	
-// for (int i=0; i<N<10;)
-//		for(int j=1; j<=M<=10;)
-//			cout << i << " " << j  << endl;
+
+	return 0;
 }
