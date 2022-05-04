d255b1490159eca27200e36498af303d02c5115e
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-03 22:42:27 -0700

    commit8

--- Quiz5-2/question4write.cpp
+++ Quiz5-2/question4write.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    ofstream ofs;
+    ofstream ofs; //
     int id;
     string name, dept;
     double num, salary;
@@ -17,5 +17,22 @@ int main()
         exit(0);
     }
 
-    cout <<
+    cout << "Number of employees: ";
+    cin >> num;
+    ofs << num << endl;
+
+    for (int i=0; i<num; i++){
+        cout << "Employee ID: ";
+        cin >> id;
+        cout << "Employee name: ";
+        cin >> name;
+        cout << "Department area: ";
+        cin >> dept;
+        cout << "Salary: ";
+        cin >> salary;
+
+        ofs << id << " " << name << " " << dept << 
+        " " << salary << endl;
+    }
+    ofs.close();
 }
