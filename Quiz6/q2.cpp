6f791d92ff3d95275d0ad31ed81919701c2e31db
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-09 19:31:29 -0700

    commit8

--- Quiz6/q2.cpp
+++ Quiz6/q2.cpp
@@ -20,4 +20,14 @@ int findMin(int n1, int n2, int n3)
     return n3;
 }
 
+void printResult(int n1, int n2, int n3, int min)
+{
+    cout << "The three random numbers are:\n" << n1 << ", " 
+    << n2 << ", " << n3 << endl;
+    cout << "The minimum value is: " << min << endl;
+}
 
+int main ()
+{
+    
+}
