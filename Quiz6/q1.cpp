bc0b662c7d7ad61e3024477b7985a2d1ef362588
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-09 17:29:38 -0700

    commit2

--- Quiz6/q1.cpp
+++ Quiz6/q1.cpp
@@ -4,8 +4,17 @@ using namespace std;
 void swapTwoValues (int n1, int n2)
 {
     int temp;
-    cout << "Enter 1st integer: " << endl;
+    cout << "Enter integer 1: " << endl;
     cin >> n1;
-    cout << "Enter 2nd integer: " << endl;
+    cout << "Enter integer 2: " << endl;
     cin >> n2;
+
+    cout << "integer 1 = " << n1 << " integer 2 = "
+    << n2 << endl;
+    temp = n1;
+    n1 = n2;
+    n2 = temp;
+
+    cout << "Swapping places, integer 1 is now " << n1 <<
+    ", and integer 2 is " << n2 << endl;
 }
