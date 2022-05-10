da37b175a8ce02e52a862affacc51f0033d869e1
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-09 19:52:35 -0700

    commit10

--- Quiz6/q2.cpp
+++ Quiz6/q2.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <stdlib.h> 
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-void genRandnum(int &n1, int &n2, int &n3)
+void getRandnum(int &n1, int &n2, int &n3)
 {
+    srand(time(0));
     n1 = rand();
     n2 = rand();
     n3 = rand();
@@ -30,9 +32,7 @@ void printResult(int n1, int n2, int n3, int min)
 int main ()
 {
     int n1, n2, n3, min;
-    genRandnum(n1,n2,n3);
-    findMin(n1, n2, n3);
+    getRandnum(n1,n2,n3);
+    min = findMin(n1, n2, n3);
     printResult(n1, n2, n3, min);
-
-    return 0;
 }
