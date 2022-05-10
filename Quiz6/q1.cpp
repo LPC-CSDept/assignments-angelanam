da878745cf8fa438136d3afab0d3bde888f56a85
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-09 17:24:27 -0700

    commit1

---
+++ Quiz6/q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void swapTwoValues (int n1, int n2)
+{
+    int temp;
+    cout << "Enter 1st integer: " << endl;
+    cin >> n1;
+    cout << "Enter 2nd integer: " << endl;
+    cin >> n2;
+}
