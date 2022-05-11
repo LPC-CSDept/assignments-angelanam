97c37b33ce8e023ad147b3bc57967d0a05889805
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-11 16:48:02 -0700

    commit2

--- Midterm/q1.cpp
+++ Midterm/q1.cpp
@@ -5,7 +5,7 @@ int main()
 {
     const  int N=5, M=5;
     int     i, j;
-    for(i=0;i<N;i++) {
+    for(i=4;i<N;i++) {
         for(j=i; j<M-i; j++)
             cout << " (" << i << "," << j << ") " ;
         cout << endl;
