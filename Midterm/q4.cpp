b9c97e14b4513efacd28cadd1e781a92a9c332e8
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-26 19:41:07 -0700

    commit12

--- Midterm/q4.cpp
+++ Midterm/q4.cpp
@@ -9,3 +9,16 @@ int getRdnum(){
     //randomizes #s between 1-100
     return rand()%101;
 }
+
+ int getRdnum(int n2){
+//this int will generate random #s, w/ one parameter of int 2
+    srand(time(0));
+    return rand()%(101-n2)+n2;
+    //returns random # between 1-n2
+    //subracting 101 by n2 & adding by n2 so values stays between the range
+ }
+
+int getRdnum(int n1, int n2){
+//this int will generate random #s,two parameters of int n1 & n2
+    srand(time(0));
+    return rand()%(n2-n1)+n1;
