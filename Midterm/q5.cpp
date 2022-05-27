ffa81f93529b7b00069397a49951f0650d60466f
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-26 20:44:57 -0700

    commit15

---
+++ Midterm/q5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <fstream>
+//using this stream to write file 
+
+using namespace std;
+
+void getInput (int num1, int num2)
+//using this function to enter 2 values
+{
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+}
