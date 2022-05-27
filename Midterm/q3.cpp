351e7ad8830c500ec402ff791ae65a614826766d
angelanam <ahnam@zonemail.clpccd.edu>
2022-05-26 18:35:54 -0700

    commit3

---
+++ Midterm/q3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <ctime>
+#include <fstream>
+//using this stream to write file
+
+using namespace std;
+
+int prevNum = 0;
+//making this variable equal 0 to keep previous value once entered
+
